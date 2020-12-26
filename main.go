d0fc7cdebe0ee0dbe3aeceaabe2a68ce6dc23ee9
Yuu <uypsbss@gmail.com>
2020-12-27 03:11:02 +0900

    add fonts

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"image/jpeg"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/julianshen/text2img"
 )
+
+func randomString() string {
+	var n uint64
+	binary.Read(rand.Reader, binary.LittleEndian, &n)
+	return strconv.FormatUint(n, 36)
+}
 
 func main() {
 	r := gin.Default()
@@ -13,6 +26,22 @@ func main() {
 		content := c.PostForm("content")
 		c.JSON(http.StatusOK, gin.H{"content": content})
 	})
+
+	path := "src/fonts/Koruri.ttf"
+  d, err := text2img.NewDrawer(text2img.Params{
+    FontPath: path,
+  })
+	fmt.Print(err)
+
+  img, err := d.Draw("text2img generates the image from a text")
+  fmt.Print(err)
+
+  file, err := os.Create("dist/"+ randomString() + ".jpg")
+  fmt.Print(err)
+  defer file.Close()
+
+  err = jpeg.Encode(file, img, &jpeg.Options{Quality: 100})
+  fmt.Print(err)
 
 	r.Run(":8080")
 }
